bb7c93c5dd9b0549c6023021738fdc0858e042e2
Arkoval <arkkowalski95@gmail.com>
2020-10-28 13:07:47 +0100

    Updates

---
+++ precache-manifest.74a6180eb983fae290e49f12b5ffd09f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bd7ef98c6cb92e18d8b4b76676d58a",
+    "url": "/giveaway-website-project/index.html"
+  },
+  {
+    "revision": "319920511d401d029053",
+    "url": "/giveaway-website-project/static/css/2.f86e83c9.chunk.css"
+  },
+  {
+    "revision": "099732c80bbdfbac046e",
+    "url": "/giveaway-website-project/static/css/main.f15ba14a.chunk.css"
+  },
+  {
+    "revision": "319920511d401d029053",
+    "url": "/giveaway-website-project/static/js/2.98cb07d4.chunk.js"
+  },
+  {
+    "revision": "a7859b41c23bed46d2a2cd4146c86afb",
+    "url": "/giveaway-website-project/static/js/2.98cb07d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099732c80bbdfbac046e",
+    "url": "/giveaway-website-project/static/js/main.f6c7f7bf.chunk.js"
+  },
+  {
+    "revision": "9f18410fff45ae4bdc33",
+    "url": "/giveaway-website-project/static/js/runtime-main.fe59c8a3.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/giveaway-website-project/static/media/3 Columns Background.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/giveaway-website-project/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/giveaway-website-project/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/giveaway-website-project/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/giveaway-website-project/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/giveaway-website-project/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/giveaway-website-project/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/giveaway-website-project/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/giveaway-website-project/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/giveaway-website-project/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/giveaway-website-project/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "c470ec0c56e643f8ee1898ea962e753a",
+    "url": "/giveaway-website-project/static/media/Icon-Arrow-Down.c470ec0c.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/giveaway-website-project/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/giveaway-website-project/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/giveaway-website-project/static/media/Signature.e54cfcfc.svg"
+  }
+]);
